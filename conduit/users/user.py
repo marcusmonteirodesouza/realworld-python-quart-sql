2a940a5c9f8e267ea5ace3cd805da6df8f6f97ba
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-02-10 17:27:18 -0500

    Make User a dataclass

--- conduit/users/user.py
+++ conduit/users/user.py
@@ -1,33 +1,12 @@
-import json
-from uuid import UUID
+from dataclasses import dataclass
 from typing import Optional
+from uuid import UUID
 
 
+@dataclass
 class User:
-    def __init__(
-        self,
-        _id: UUID,
-        username: str,
-        email: str,
-        bio: Optional[str],
-        image: Optional[str],
-    ):
-        self._id = _id
-        self.username = username
-        self.email = email
-        self.bio = bio
-        self.image = image
-
-    def __repr__(self):
-        return f"User({str(self._id)}, {self.username}, {self.email}, {self.bio}, {self.image})"
-
-    def __str__(self):
-        return json.dumps(
-            {
-                "_id": str(self._id),
-                "username": self.username,
-                "email": self.email,
-                "bio": self.bio,
-                "image": self.image,
-            }
-        )
+    _id: UUID
+    username: str
+    email: str
+    bio: Optional[str]
+    image: Optional[str]
