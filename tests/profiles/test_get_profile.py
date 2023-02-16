4e06c5cf54d6b9f52ce3d2df4ae828e78bd60ab8
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2023-02-16 00:10:38 -0500

    Add tests

--- tests/profiles/test_get_profile.py
+++ tests/profiles/test_get_profile.py
@@ -128,3 +128,78 @@ async def test_when_valid_token_and_follower_is_not_found_should_return_401(
     response_data = await response.json
 
     assert response_data["errors"]["body"][0] == "unauthorized"
+
+
+@pytest.mark.asyncio
+async def test_when_authorization_header_has_invalid_scheme_should_return_200(
+    app, create_user, follow_user
+):
+    client = app.test_client()
+
+    follower = await create_user()
+
+    followed = await create_user()
+
+    await follow_user(follower_token=follower.token, username=followed.username)
+
+    response = await client.get(
+        make_get_profile_url(username=followed.username),
+        headers={"Authorization": f"Bearer {follower.token}"},
+    )
+
+    assert response.status_code == 200
+
+    response_data = await response.json
+
+    profile = response_data["profile"]
+
+    assert profile["username"] == followed.username
+    assert profile["bio"] == followed.bio
+    assert profile["image"] == followed.image
+    assert not profile["following"]
+
+
+@pytest.mark.asyncio
+async def test_when_token_has_invalid_signature_should_return_401(app, create_user):
+    client = app.test_client()
+
+    follower = await create_user()
+
+    followed = await create_user()
+
+    secret_key = secrets.token_urlsafe()
+
+    token = create_jwt(username=follower.username, secret_key=secret_key)
+
+    response = await client.get(
+        make_get_profile_url(username=followed.username),
+        headers={"Authorization": f"Token {token}"},
+    )
+
+    assert response.status_code == 401
+
+    response_data = await response.json
+
+    assert response_data["errors"]["body"][0] == "unauthorized"
+
+
+@pytest.mark.asyncio
+async def test_when_token_is_expired_should_return_401(app, create_user):
+    client = app.test_client()
+
+    follower = await create_user()
+
+    followed = await create_user()
+
+    token = create_jwt(username=follower.username, expires_seconds=-1)
+
+    response = await client.get(
+        make_get_profile_url(username=followed.username),
+        headers={"Authorization": f"Token {token}"},
+    )
+
+    assert response.status_code == 401
+
+    response_data = await response.json
+
+    assert response_data["errors"]["body"][0] == "unauthorized"
